1d7eea27e3810110c52a896b131ca6be8fd62edf
Aldo Wanderley Costa Junior <aldowanderley11@gmail.com>
2024-01-11 23:35:30 -0300

    :test_tube: test: fix the async component problem

---
+++ app/libs/requests.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from "next/server";
+import { RestaurantResponse } from "./types";
+
+export const getRestaurants = async (): Promise<RestaurantResponse> => {
+  const response = await fetch('http://localhost:3000/restaurants') as NextResponse<RestaurantResponse>;
+  if (!response.ok) {
+    throw new Error('Error in get restaurants')
+  }
+  return response.json();
+}
